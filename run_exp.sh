5984f7dfffb74c696ae2dfd5b039f44797cb446d
Marina <marina.crespo@alumnos.uc3m.es>
2024-09-27 21:20:14 +0200

    another commit

--- run_exp.sh
+++ run_exp.sh
@@ -10,15 +10,16 @@ source /scratch_net/ken/mcrespo/conda/etc/profile.d/conda.sh # TODO: SET.
 conda activate mri_reconstruction
 
 
-# # NOTE: Uncomment when running multi-gpu script.
-# export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-# echo "MASTER_PORT="$MASTER_PORT
+# NOTE: Uncomment when running multi-gpu script.
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+echo "MASTER_PORT="$MASTER_PORT
 
-# master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-# export MASTER_ADDR=$master_addr
-# echo "MASTER_ADDR="$MASTER_ADDR
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
 
 
 # python -u multi_gpu/main.py
 # python -u multi_vol/main.py
-python -u single_vol/main.py
+# python -u single_vol/main.py
+python -u single_vol_multigpu/main.py
